4ac11983a9bd0721e8142715125fead46f9c8d22
abhishekyt <abhishekyt@CCRL-L-013.ccrl.local>
2025-12-24 18:20:34 +0530

    24-Dec-2025

--- src/test/java/TestCase/CR318_Reports_WSPadmin_WH_login_and_Assayer_login_TestCase.java
+++ src/test/java/TestCase/CR318_Reports_WSPadmin_WH_login_and_Assayer_login_TestCase.java
@@ -14,16 +14,17 @@ import TestPages.LoginPages;
 import TestPages.Physical_Deposit_Maker;
 import TestPages.Pledge_Creation_WareHouse;
 import TestPages.Withdrawal_Confirm_WSP_Maker;
-//@Listeners(Utillity.TestNGListeners.class)
-/*public class CR318_Reports_WSPadmin_WH_login_and_Assayer_login_TestCase extends BaseClass {
+
+@Listeners(Utillity.TestNGListeners.class)
+public class CR318_Reports_WSPadmin_WH_login_and_Assayer_login_TestCase extends BaseClass {
 
 	ExtentReports Report = new ExtentReports(
-			"C:\\Users\\abhishekyt\\git\\repository\\Automation\\Reports\\CR318_"
+			"C:\\Users\\abhishekyt\\git\\repository\\Automation\\Reports\\CR318_24_Dec"
 					+ System.currentTimeMillis() + ".html",
 			true);
 	ExtentTest test = Report.startTest("CR318:Reports WSPadmin WH login and Assayer login");
 	
-	//@Test
+	@Test
 	  void Pledge_Creation_WareHouse_CR318 () throws IOException {
 		try {
 			test.log(LogStatus.INFO, "Pledge_Creation_WareHouse_CR318 is start");
@@ -32,14 +33,14 @@ import TestPages.Withdrawal_Confirm_WSP_Maker;
 			Login.PortalLogin("WSP9999996", "user71", "121@test");
 			Pledge_Creation_WareHouse CR118 = new Pledge_Creation_WareHouse(driver,Wait);
 			CR118.Pledge_Creation_WareHouse_Approval_C118();
-			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver, "Pledge_Creation_WareHouse_CR318"))
+			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver))
 					+ "Pledge_Creation_WareHouse_CR318 is successfull");
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(45));
 			TestPages.LogoutPage Log = new TestPages.LogoutPage(driver, Wait);
 			Log.Logout();
 		} catch (Exception e) {
 			test.log(LogStatus.FAIL,
-					test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver, "Pledge_Creation_WareHouse_CR318")) + "Test failed for Pledge_Creation_WareHouse_CR318: "+e.getMessage());
+					test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver)) + "Test failed for Pledge_Creation_WareHouse_CR318: "+e.getMessage());
 		}
 		Report.endTest(test);
 		Report.flush();
@@ -54,14 +55,14 @@ import TestPages.Withdrawal_Confirm_WSP_Maker;
 			Login.PortalLogin("WSP9999996", "user71", "121@test");
 			Physical_Deposit_Maker CR118 = new Physical_Deposit_Maker(driver,Wait);
 			CR118.Physical_Deposit_Pending_CR118();
-			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver, "Physical_Deposit_Pending_CR318"))
+			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver))
 					+ "Physical_Deposit_Pending_CR318 is successfull");
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(45));
 			TestPages.LogoutPage Log = new TestPages.LogoutPage(driver, Wait);
 			Log.Logout();
 		} catch (Exception e) {
 			test.log(LogStatus.FAIL,
-					test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver, "Physical_Deposit_Pending_CR318")) + "Test failed for Physical_Deposit_Pending_CR318: "+e.getMessage());
+					test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver)) + "Test failed for Physical_Deposit_Pending_CR318: "+e.getMessage());
 		}
 		Report.endTest(test);
 		Report.flush();
@@ -76,14 +77,14 @@ import TestPages.Withdrawal_Confirm_WSP_Maker;
 			Login.PortalLogin("WSP9999996", "user71", "121@test");
 			Withdrawal_Confirm_WSP_Maker CR118 = new Withdrawal_Confirm_WSP_Maker(driver,Wait);
 			CR118.Withdrawal_Confirm_Pending_CR118();
-			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver, "Withdrawal_Confirm_Pending_CR318"))
+			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver))
 					+ "Withdrawal_Confirm_Pending_CR318 is successfull");
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(45));
 			TestPages.LogoutPage Log = new TestPages.LogoutPage(driver, Wait);
 			Log.Logout();
 		} catch (Exception e) {
 			test.log(LogStatus.FAIL,
-					test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver, "Withdrawal_Confirm_Pending_CR318")) + "Test failed for Withdrawal_Confirm_Pending_CR318: "+e.getMessage());
+					test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver)) + "Test failed for Withdrawal_Confirm_Pending_CR318: "+e.getMessage());
 		}
 		Report.endTest(test);
 		Report.flush();
@@ -98,20 +99,20 @@ import TestPages.Withdrawal_Confirm_WSP_Maker;
 			Login.PortalLogin("WSP9999996", "user71", "121@test");
 			Deposit_Confirm_WH_Maker CR118 = new Deposit_Confirm_WH_Maker(driver,Wait);
 			CR118.Deposit_Confirm_Pending_CR118();
-			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver, "Deposit_Confirm_Pending_CR318"))
+			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver))
 					+ "Deposit_Confirm_Pending_CR318 is successfull");
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(45));
 			TestPages.LogoutPage Log = new TestPages.LogoutPage(driver, Wait);
 			Log.Logout();
 		} catch (Exception e) {
 			test.log(LogStatus.FAIL,
-					test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver, "Deposit_Confirm_Pending_CR318")) + "Test failed for Deposit_Confirm_Pending_CR318: "+e.getMessage());
+					test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver)) + "Test failed for Deposit_Confirm_Pending_CR318: "+e.getMessage());
 		}
 		Report.endTest(test);
 		Report.flush();
 	} 
 	
-	@Test
+//	@Test
 	  void Deposit_Assayer_Pending_CR318 () throws IOException {
 		try {
 			test.log(LogStatus.INFO, "Deposit_Assayer_Pending_CR318 is start");
@@ -121,17 +122,16 @@ import TestPages.Withdrawal_Confirm_WSP_Maker;
 			
 			Deposite_Assayer_Maker CR118 = new Deposite_Assayer_Maker(driver,Wait);
 			CR118.Deposit_Assayer_Pending_CR318();
-			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver, "Deposit_Assayer_Pending_CR318"))
+			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver))
 					+ "Deposit_Assayer_Pending_CR318 is successfull");
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(45));
 			TestPages.LogoutPage Log = new TestPages.LogoutPage(driver, Wait);
 			Log.Logout();
 		} catch (Exception e) {
 			test.log(LogStatus.FAIL,
-					test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver, "Deposit_Assayer_Pending_CR318")) + "Test failed for Deposit_Assayer_Pending_CR318:"+e.getMessage());
+					test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver)) + "Test failed for Deposit_Assayer_Pending_CR318:"+e.getMessage());
 		}
 		Report.endTest(test);
 		Report.flush();
 	} 
 }
-*/
--- src/test/java/TestCase/General_Deposite_Agriculture_TestCase.java
+++ src/test/java/TestCase/General_Deposite_Agriculture_TestCase.java
@@ -34,7 +34,8 @@ public class General_Deposite_Agriculture_TestCase extends BaseClass {
 
 			LoginPages Login = new LoginPages(driver, Wait);
 		//Login.PortalLogin("rp-Abhishek", "user51", "121@test");
-			Login.PortalLogin("rp-KML", "user1", "121@test");
+			Login.PortalLogin("RP-KUNVARJI", "PRAKASH", "121@test");
+			//Login.PortalLogin("rp-KML", "user1", "121@test");
 			Depo.General_Deposite_Request();
 			test.log(LogStatus.PASS, test.addScreenCapture(TestPages.ScreenShort.CaptureScreen(driver))
 					+ "RP_Deposite_Request_Maker is success full");
